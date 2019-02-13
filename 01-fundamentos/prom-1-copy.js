319115436ee4bf8fd966327f9573eab1fd3851f1
Gustavo Herrera <gustavoh.2312@gmail.com>
2019-02-12 21:24:12 -0500

    promesas

---
+++ 01-fundamentos/prom-1-copy.js
@@ -0,0 +1,50 @@
+
+function sumarUno( numero, callback )
+{
+
+	if ( numero >= 7) {
+		callback('numero muy alto');
+		return;
+	}
+
+	setTimeout( function() {
+			
+			callback(null, numero + 1);
+	}, 800);
+	
+}
+
+
+//console.log(sumarUno(5));
+sumarUno(5, function(error, nuevoValor) {
+
+	if(error)
+	{
+		console.log(error);
+		return;
+	}
+
+	sumarUno( nuevoValor, function(error, nuevoValor2) {
+
+
+			if(error)
+			{
+				console.log(error);
+				return;
+			}
+			
+		sumarUno( nuevoValor2, function(error, nuevoValor3) {
+
+			
+			if(error)
+			{
+				console.log(error);
+				return;
+			}
+				console.log(nuevoValor3);
+
+		});
+
+	});
+
+});
